f413a5169992af8e35fe95d89597a95b9827ff87
Ketan Shukla <ketankshukla@gmail.com>
2023-04-15 18:14:53 -0700

    Change 2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App()   {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React With Ketan
+          Learn React With Ketan Today!
         </a>
       </header>
     </div>
